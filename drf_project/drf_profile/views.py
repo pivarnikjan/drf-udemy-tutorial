be3f2e6ec4e7c61b822677ea245e28b538faa955
Jan Pivarnik <pivarnikjan@gmail.com>
2017-07-20 20:50:43 +0200

    [ADD] introduction to ViewSets

--- drf_project/drf_profile/views.py
+++ drf_project/drf_profile/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -48,3 +48,52 @@ class HelloApiView(APIView):
         """ Deletes and object. """
 
         return Response({'method': 'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """ Test API ViewSet """
+
+    serializer_class = serializers.HelloApiViewSerializer
+
+    def list(self, request):
+        """ Return a hello message. """
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update, destroy)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code.'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """ Create a new hello message. """
+
+        serializer = serializers.HelloApiViewSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = "Hello {0}".format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """ Handles getting an object by its ID. """
+
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """ Handles updating an object. """
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """ Handles updating part of an object. """
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """ Handles destroying an object. """
+
+        return Response({'http_method': 'DESTROY'})
